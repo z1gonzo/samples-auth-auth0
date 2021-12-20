2c2a1f1758c91e03973c69ac5c3af879b01c279b
z1gonzo <goonsky@gmail.com>
2021-12-20 11:31:59 +0100

    basic express api with psql

--- server/src/app.js
+++ server/src/app.js
@@ -8,6 +8,8 @@ require('dotenv').config();
 const middlewares = require('./middlewares');
 const api = require('./api');
 
+const { pool } = require('./db/connection');
+
 const app = express();
 
 app.use(morgan('dev'));
@@ -15,11 +17,69 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
+// CREATE TODO
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query("INSERT INTO todos (description) VALUES($1) RETURNING *",
+      [description]);
+    res.json(newTodo.rows[0]);
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
+//GET ALL TODOS
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todos");
+    res.json(allTodos.rows)
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
+//GET A TODO
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todos WHERE todo_id = $1",
+      [id]);
+    res.json(todo)
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
+//UPDATE A TODO
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query("UPDATE todos SET description = $1 WHERE todo_id = $2",
+      [description, id]);
+    res.json("Todo was updated!")
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
+//DELETE A TODO
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("DELETE FROM todos WHERE todo_id = $1",
+      [id]);
+    res.json("Todo was deleted!")
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
 app.get('/', (req, res) => {
-  res.json({
-    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄'
-  });
+  res.json({ info: 'Node.js, Express, and Postgres API' });
 });
+
 
 app.use('/api/v1', api);
 
---
+++ server/src/db/queries.js
@@ -0,0 +1,17 @@
+//CREATE A TODO
+
+app.post("/todos", async (req, res) => {
+  try {
+    console.log(req.body)
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
+//GET ALL TODOS
+
+//GET A TODO
+
+//UPDATE A TODO
+
+//DELETE A TODO
